9eb5fe43fd880d05e2a76a7ba3b8691e27cf3d85
mohamed-groun <44194988+mohamed-groun@users.noreply.github.com>
2018-11-05 11:06:06 +0100

    Add files via upload

---
+++ projetfile.php
@@ -0,0 +1,14 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+ <p> <?php echo "Ceci est une ligne créée uniquement en PHP" ?></p>
+
+ <p> <?php echo  "Ceci est la 2ème phrase créée avec PHP" ?>  </p>
+    <a  href="<?php echo 'https://gomycode.tn/' ?>">GOMYCODE</a>
+</body>
+</html>
